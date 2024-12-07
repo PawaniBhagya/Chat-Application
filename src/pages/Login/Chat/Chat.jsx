2d5e6f4eef16e04c056f5f350960c356da0cbf87
pawani <pawanibhagya10@gmail.com>
2024-12-07 22:46:11 +0530

    adding dev branch

--- src/pages/Login/Chat/Chat.jsx
+++ src/pages/Login/Chat/Chat.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import "./Chat.css";
+import React from 'react'
+import './Chat.css'
+import LeftSideBar from '../../../components/LeftSideBar/LeftSideBar'
+import ChatBox from '../../../components/ChatBox/ChatBox'
+import RightSideBar from '../../../components/RightSideBar/RightSideBar'
 
 const Chat = () => {
-  return(
-    <div>
-       
+  return (
+    <div className='Chat'>
+      <div className="Chat-container">
+        <LeftSideBar />
+        <ChatBox />
+        <RightSideBar />
+      </div>
     </div>
 
   )
- 
 }
 
 export default Chat;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 
 const Login = () => {
+
+    const [currState,setCurrState] = useState("Sign Up");
   return (
   <div className='Login'>
     <img src={assets.logo_big} alt="" className="logo" />
-    <form className="login-form">
-        <h2>Sign Up</h2>
-        <input type="text" placeholder="username" className="form-input" required/>
-        <input type="text" placeholder="email address"className="form-input" />
-        <input type="text" placeholder="password"className="form-input" />
-        <button type="submit">Sign Up</button>
-        <div className="login-term">
+    <form className="Login-form">
+        <h2>{currState}</h2>
+        {currState === "Sign Up"?
+        <input type="text" placeholder="username" className="form-input" required/> :null}
+        <input type="text" placeholder="email address"className="form-input" required/>
+        <input type="text" placeholder="password"className="form-input" required/>
+        <button type="submit">{currState ==="Sign Up"?"Create account":"Login now"}</button>
+        <div className="Login-term">
             <input type="checkbox" />
             <p>Agree to the terms of use & privacy policy.</p>
         </div>
-        <div className="login-forgot">
-            <p className="login-toggle">Already have an account <span></span>click here.</p>
-        </div>
+        <div className="Login-forgot">
+            {
+                currState === "Sign Up"
+                ?<p className="Login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here.</span></p> :
+                <p className="Login-toggle">Create an account <span onClick={()=>setCurrState("Sign Up")}>click here.</span></p>
+     
+            }
+            
+               </div>
     </form>
   </div>  
   
